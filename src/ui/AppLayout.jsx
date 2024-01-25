7f3f2a798ab9594b281d64ac0e10c4d99ef9d56d
Royal Dev <157358195+royaldev1031@users.noreply.github.com>
2024-01-25 04:42:41 -0500

    install tailwind prettier, working with color

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,11 +1,11 @@
-import Header from './Header'
-import Loader from './Loader'
-import CartOverview from '../features/cart/CartOverview'
-import { Outlet, useNavigation } from 'react-router-dom'
+import Header from './Header';
+import Loader from './Loader';
+import CartOverview from '../features/cart/CartOverview';
+import { Outlet, useNavigation } from 'react-router-dom';
 
 function AppLayout() {
-  const navigation = useNavigation()
-  const isLoading = navigation.state === 'loading'
+  const navigation = useNavigation();
+  const isLoading = navigation.state === 'loading';
 
   return (
     <div className="layout">
@@ -19,7 +19,7 @@ function AppLayout() {
 
       <CartOverview />
     </div>
-  )
+  );
 }
 
-export default AppLayout
+export default AppLayout;
