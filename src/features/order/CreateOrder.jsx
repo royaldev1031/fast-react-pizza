1bf805c46b901c8354138ffb1e0adf0ece2f4648
Royal Dev <157358195+royaldev1031@users.noreply.github.com>
2024-01-26 04:33:28 -0500

    styling buttons, element states and transitions

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
-import { Form, redirect, useActionData, useNavigation } from 'react-router-dom'
-import { createOrder } from '../../services/apiRestaurant'
+import { useState } from 'react';
+import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
+import { createOrder } from '../../services/apiRestaurant';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
-    str
-  )
+    str,
+  );
 
 const fakeCart = [
   {
@@ -30,16 +30,16 @@ const fakeCart = [
     unitPrice: 15,
     totalPrice: 15,
   },
-]
+];
 
 function CreateOrder() {
-  const navigation = useNavigation()
-  const isSubmitting = navigation.state === 'submitting'
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === 'submitting';
 
-  const formErrors = useActionData()
+  const formErrors = useActionData();
 
   // const [withPriority, setWithPriority] = useState(false);
-  const cart = fakeCart
+  const cart = fakeCart;
 
   return (
     <div>
@@ -80,36 +80,40 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <button disabled={isSubmitting}>
+          <button
+            disabled={isSubmitting}
+            className="inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed"
+          >
             {isSubmitting ? 'Placing order...' : 'Order now'}
           </button>
         </div>
       </Form>
     </div>
-  )
+  );
 }
 
 export async function action({ request }) {
-  const formData = await request.formData()
-  const data = Object.fromEntries(formData)
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
 
   const order = {
     ...data,
     cart: JSON.parse(data.cart),
     priority: data.priority === 'on',
-  }
+  };
 
-  const errors = {}
+  const errors = {};
   if (!isValidPhone(order.phone))
     errors.phone =
-      'Please give us your correct phone number. We might need it to contact you.'
+      'Please give us your correct phone number. We might need it to contact you.';
 
-  if (Object.keys(errors).length > 0) return errors
+  if (Object.keys(errors).length > 0) return errors;
 
   // If everything is okay, create new order and redirect
-  const newOrder = await createOrder(order)
+  // const newOrder = await createOrder(order);
 
-  return redirect(`/order/${newOrder.id}`)
+  // return redirect(`/order/${newOrder.id}`);
+  return null;
 }
 
-export default CreateOrder
+export default CreateOrder;
